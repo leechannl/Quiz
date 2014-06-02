a5a5b233f79d4254615c4721b4f5b7bf2e8b006f
leechannl <leechannl@gmail.com>
2014-06-03 00:52:17 +0800

    Initial Commit

---
+++ Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by mm on 6/3/14.
+//  Copyright (c) 2014 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
